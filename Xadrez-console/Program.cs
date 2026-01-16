72ba22072099e65c9d2fc8df70983fdc8908764d
Théo <theocs2211@gmail.com>
2026-01-16 15:56:58 -0300

    Classe posicao criada

--- Xadrez-console/Program.cs
+++ Xadrez-console/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using XadrezConsole.Tabuleiro;
+
+namespace xadrezConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Posicao p = new Posicao(3, 4);
+
+            Console.WriteLine("Posição:" + p);
+        }
+    }
+}
