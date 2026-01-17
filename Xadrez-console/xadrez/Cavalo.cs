290c98bbb8493dfbf6cb7371e0834cdd3908c082
Théo <theocs2211@gmail.com>
2026-01-17 18:43:00 -0300

    Todas as peças adicionadas

---
+++ Xadrez-console/xadrez/Cavalo.cs
@@ -0,0 +1,66 @@
+﻿using XadrezConsole.tabuleiro;
+
+namespace XadrezConsole.xadrez
+{
+    internal class Cavalo : Peca
+    {
+        public Cavalo(Cor cor, Tabuleiro tab) : base(cor, tab)
+        {
+        }
+
+        public override string ToString()
+        {
+            return "C";
+        }
+
+        public override bool[,] MovimentosPossiveis()
+        {
+            bool[,] mat = new bool[Tab.Linhas, Tab.Colunas];
+
+            Posicao pos = new Posicao(0, 0);
+
+            pos.DefinirPosicao(Pos.Linha - 2, Pos.Coluna - 1);
+            if (Tab.PosicaoValida(pos) && PodeMover(pos))
+            {
+                mat[pos.Linha, pos.Coluna] = true;
+            }
+            pos.DefinirPosicao(Pos.Linha - 2, Pos.Coluna + 1);
+            if (Tab.PosicaoValida(pos) && PodeMover(pos))
+            {
+                mat[pos.Linha, pos.Coluna] = true;
+            }
+            pos.DefinirPosicao(Pos.Linha - 1, Pos.Coluna + 2);
+            if (Tab.PosicaoValida(pos) && PodeMover(pos))
+            {
+                mat[pos.Linha, pos.Coluna] = true;
+            }
+            pos.DefinirPosicao(Pos.Linha + 1, Pos.Coluna + 2);
+            if (Tab.PosicaoValida(pos) && PodeMover(pos))
+            {
+                mat[pos.Linha, pos.Coluna] = true;
+            }
+            pos.DefinirPosicao(Pos.Linha + 2, Pos.Coluna - 1);
+            if (Tab.PosicaoValida(pos) && PodeMover(pos))
+            {
+                mat[pos.Linha, pos.Coluna] = true;
+            }
+            pos.DefinirPosicao(Pos.Linha + 2, Pos.Coluna + 1);
+            if (Tab.PosicaoValida(pos) && PodeMover(pos))
+            {
+                mat[pos.Linha, pos.Coluna] = true;
+            }
+            pos.DefinirPosicao(Pos.Linha - 1, Pos.Coluna - 2);
+            if (Tab.PosicaoValida(pos) && PodeMover(pos))
+            {
+                mat[pos.Linha, pos.Coluna] = true;
+            }
+            pos.DefinirPosicao(Pos.Linha + 1, Pos.Coluna - 2);
+            if (Tab.PosicaoValida(pos) && PodeMover(pos))
+            {
+                mat[pos.Linha, pos.Coluna] = true;
+            }
+
+            return mat;
+        }
+    }
+}
